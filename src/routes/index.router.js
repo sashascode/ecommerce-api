aa49e9a6596169923e1c4f2401bb8b0850ffc101
sashascode <racagnisasha@gmail.com>
2024-02-22 21:59:40 -0300

    Rename router folders

--- src/routes/index.router.js
+++ src/routes/index.router.js
@@ -1,6 +1,6 @@
-export { default as ProductRouter } from './products.router.js';
+export { default as ProductRouter } from './product.router.js';
 export { default as UserRouter } from './user.router.js';
-export { default as CartRouter } from './carts.router.js';
+export { default as CartRouter } from './cart.router.js';
 export { default as MockRouter } from './mock.router.js';
-export { default as ViewRouter } from './views.router.js';
+export { default as ViewRouter } from './view.router.js';
 export { default as TokenRouter } from './token.router.js';
