26bffaea2587ee8ab75c85c9c4035821d4537096
sashascode <racagnisasha@gmail.com>
2024-03-18 19:39:34 -0300

    Add documents and last_connection properties to user model

--- src/dao/mongo/models/user.model.js
+++ src/dao/mongo/models/user.model.js
@@ -4,6 +4,11 @@ import { CartModel } from './cart.model.js';
 
 const userCollection = 'users';
 
+const documentSchema = new mongoose.Schema({
+    name: { type: String, required: true },
+    reference: { type: String, required: true }
+});
+
 const userSchema = new mongoose.Schema(
     {
         first_name: { type: String, required: [true, "Name is required"] },
@@ -14,7 +19,9 @@ const userSchema = new mongoose.Schema(
         status: { type: Boolean, default: true },
         creationDate: { type: Date, default: Date.now },
         cart: { type: mongoose.Schema.Types.ObjectId, ref: CartModel },
-        age: { type: Number, min: 18, max: 65 }
+        age: { type: Number, min: 18, max: 65 },
+        documents: { type: [ documentSchema ], default: [] },
+        last_connection: { type: Date, default: Date.now }
     }
 );
 
@@ -23,6 +30,11 @@ userSchema.set('toJSON', {
         delete ret.__v;
         return ret;
     }
+});
+
+userSchema.pre('findOneAndUpdate', function(next) {
+    this.update({}, { last_connection: new Date() });
+    next();
 });
 
 export const UserModel = mongoose.model(userCollection, userSchema);
